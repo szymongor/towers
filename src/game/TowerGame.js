de85412c087020aaa3b3096edac2723b4ae72e4f
szymongor <gornioczek.szymon@gmail.com>
2021-05-05 20:15:46 +0200

    move camera

--- src/game/TowerGame.js
+++ src/game/TowerGame.js
@@ -5,26 +5,56 @@ class TowerGame extends Phaser.Scene
 {
     constructor () {
         super();
+        this.map = {};
+        this.towers = [];
+        this.iter = 0;
     }
 
     //load assets
     preload () {
-        this.load.image('tower',towerPng)
+        this.load.image('tower',towerPng);
 
     }
       
     create () {
-        this.tower = this.add.sprite(400, 180, 'tower' );
-        this.tower.scale =0.2;
+        this.camera = this.cameras.main.setSize(800, 600);
+        // this.tower = this.add.sprite(400, 180, 'tower' );
+        // this.tower.scale =0.2;
+        this.drawMap(this.registry.map);
     }
 
     update() {
-        this.tower.angle += 1;
+        // var halfWidth = 800 / 2;
+        // var quarterWidth = halfWidth / 2;
+        // var halfHeight = 600 / 2;
+        // var quarterHeight = halfHeight / 2;
 
+        // this.camera.scrollX = (halfWidth - quarterWidth + (Math.cos(this.iter) * quarterWidth))|0;
+        // this.camera.scrollY = (halfHeight - quarterHeight + (Math.sin(this.iter) * quarterHeight))|0;
+
+        // this.iter += 0.02;
+        if (this.game.input.activePointer.isDown) {	
+            if (this.game.origDragPoint) {		
+                // move the camera by the amount the mouse has moved since last update		
+                this.camera.scrollX += this.game.origDragPoint.x - this.game.input.activePointer.position.x;		
+                this.camera.scrollY += this.game.origDragPoint.y - this.game.input.activePointer.position.y;	}	
+                // set new drag origin to current position	
+                this.game.origDragPoint = this.game.input.activePointer.position.clone();}
+                else {	this.game.origDragPoint = null;}
+    }
+
+    drawMap(map) {
+        console.log(map);
+        map.forEach(mapElement => {
+            console.log("x: "+ mapElement.x);
+            var tower = this.add.sprite(mapElement.x, mapElement.y, 'tower' );
+            tower.scale =0.2;
+    
+        });
     }
 }
 
-const createTowerGame = () => {
+const createTowerGame = (map) => {
 
     const config = {
         type: Phaser.AUTO,
@@ -35,6 +65,8 @@ const createTowerGame = () => {
     };
 
     const game = new Phaser.Game(config);
+
+    game.registry.map = map;
 
     return game;
 
--- src/index.js
+++ src/index.js
@@ -2,8 +2,17 @@
 
 import { createTowerGame } from './game/TowerGame';
 
-
-const towerGame = createTowerGame();
+var map = [
+  {x: 50, y: 50},
+  {x: 300, y: 350},
+  {x: 500, y: 350},
+  {x: 600, y: 250},
+  {x: 900, y: 900},
+  {x: 500, y: 600},
+  {x: 800, y: 800},
+  {x: 200, y: 150}
+]
+const towerGame = createTowerGame(map);
 
 
 
