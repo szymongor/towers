b999afbbad46a5a256d0f4e2b2fac2db2f3ed976
szymongor <gornioczek.szymon@gmail.com>
2021-12-21 22:23:15 +0100

    Refactor map factory

--- src/game/engine/map/MapFactory.ts
+++ src/game/engine/map/MapFactory.ts
@@ -1,8 +1,7 @@
 import { GameEngine } from "../GameEngine";
-import { UnitFactory, UnitName } from "../units/UnitFactory";
-import { UnitStorage } from "../units/UnitsStorage";
+import { UnitName } from "../units/UnitFactory";
 import { MapBoard } from "./MapBoard";
-
+import { initBasicMap } from "./maps/basic/basicMap";
 
 class MapFactory {
 
@@ -17,46 +16,13 @@ class MapFactory {
     }
 
     basicMapInit(gameEngine: GameEngine) {
-        var basicMap = new MapBoard(1000 ,1000, gameEngine);
+        var basicMap = initBasicMap(gameEngine);
         this.maps.set('basic', basicMap);
-        this.basicMapInitAddStartBuildings(gameEngine);
 
         return basicMap;
     }
 
-    basicMapInitAddStartBuildings(gameEngine: GameEngine) {
-        let unitFactory = gameEngine.unitFactory;
-        let unitStorage = gameEngine.unitStorage;
-        let eventRegistry = gameEngine.events;
-        let p1 = gameEngine.players[0];
-        let p2 = gameEngine.players[1];
-        let units = [];
-        units.push(unitFactory.of(UnitName.CASTLE,720, 720, eventRegistry, p2));
-        units.push(unitFactory.of(UnitName.CASTLE,100, 100, eventRegistry, p1));
-
-        let stones = [{x: 360, y: 140}, {x: 320, y: 320}, {x: 140, y: 360}];
-
-        let trees = [{x: 140, y:460}, {x: 300, y: 460}, {x: 60, y:540}, {x: 500, y:60}, {x: 600, y: 80}];
-
-        let towersP2 = [{x: 540, y: 820}, {x: 600, y: 640}, {x: 760, y: 540}, 
-            {x: 200, y: 660}, {x: 760, y: 540}, {x: 800, y: 120},
-            {x: 540, y:420}
-        ];
-
-        stones.forEach(s => {
-            units.push(unitFactory.of(UnitName.STONES,s.x, s.y, eventRegistry))
-        })
-
-        trees.forEach(s => {
-            units.push(unitFactory.of(UnitName.TREE,s.x, s.y, eventRegistry))
-        })
-
-        towersP2.forEach(s => {
-            units.push(unitFactory.of(UnitName.TOWER,s.x, s.y, eventRegistry, p2))
-        })
-
-        unitStorage.addUnits(units);
-    }
+    
 
 
 
---
+++ src/game/engine/map/maps/basic/basicMap.ts
@@ -0,0 +1,47 @@
+import { GameEngine } from "../../../GameEngine";
+import { UnitName } from "../../../units/UnitFactory";
+import { MapBoard } from "../../MapBoard";
+
+const initBasicMap  = (gameEngine: GameEngine) => {
+    var basicMap = new MapBoard(1000 ,1000, gameEngine);
+    basicMapInitAddStartBuildings(gameEngine);
+
+    return basicMap;
+}
+
+const basicMapInitAddStartBuildings = (gameEngine: GameEngine) => {
+    let unitFactory = gameEngine.unitFactory;
+    let unitStorage = gameEngine.unitStorage;
+    let eventRegistry = gameEngine.events;
+    let p1 = gameEngine.players[0];
+    let p2 = gameEngine.players[1];
+    let units = [];
+    units.push(unitFactory.of(UnitName.CASTLE,720, 720, eventRegistry, p2));
+    units.push(unitFactory.of(UnitName.CASTLE,100, 100, eventRegistry, p1));
+
+    let stones = [{x: 360, y: 140}, {x: 320, y: 320}, {x: 140, y: 360}];
+
+    let trees = [{x: 140, y:460}, {x: 300, y: 460}, {x: 60, y:540}, 
+        {x: 500, y:60}, {x: 600, y: 80}, {x: 220, y: 340}, {x: 460, y: 320}];
+
+    let towersP2 = [{x: 540, y: 820}, {x: 600, y: 640}, {x: 760, y: 540}, 
+        {x: 200, y: 660}, {x: 760, y: 540}, {x: 800, y: 120},
+        {x: 540, y:420}
+    ];
+
+    stones.forEach(s => {
+        units.push(unitFactory.of(UnitName.STONES,s.x, s.y, eventRegistry))
+    })
+
+    trees.forEach(s => {
+        units.push(unitFactory.of(UnitName.TREE,s.x, s.y, eventRegistry))
+    })
+
+    towersP2.forEach(s => {
+        units.push(unitFactory.of(UnitName.TOWER,s.x, s.y, eventRegistry, p2))
+    })
+
+    unitStorage.addUnits(units);
+}
+
+export { initBasicMap }
