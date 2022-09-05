60761da947b7b1450184090862e26bd64e8004e4
szymongor <gornioczek.szymon@gmail.com>
2022-09-05 23:02:28 +0200

    GameEngine update test

---
+++ src/test/game/engine/GameEngine.spec.ts
@@ -0,0 +1,40 @@
+import { AiProcessor } from "../../../game/engine/Ai/processor/AiProcessor";
+import { Campaign } from "../../../game/engine/campaign/Campaign";
+import { GameEngine } from "../../../game/engine/GameEngine"
+import { MapBoard, Terrain, TerrainType } from "../../../game/engine/map/MapBoard";
+import { Player } from "../../../game/engine/Player";
+import { logGameFinishedEvent, registerGameFinishedRule } from "../../../game/engine/rules/game_finished/GameFinishedRule";
+import { registerOrderBuildingRule } from "../../../game/engine/rules/order_building/OrderBuilding";
+import { registerPlayerLostRule } from "../../../game/engine/rules/player_lost/PlayerLostRule";
+import { registerUnitDestroyedRule } from "../../../game/engine/rules/unit_destroyed/UnitDestroyedRule";
+
+const testCampaignProvider = (gameEngine: GameEngine) => {
+    let players = [new Player('1', 'Player1'), new Player('2', 'Bot')];
+
+    let terrain: Terrain = { type: (x: number, y: number) => TerrainType.DEFAULT }
+    let mapSupplier = () => new MapBoard(100, 100, gameEngine, terrain);
+
+    let aiProcessor =  new AiProcessor(gameEngine);
+
+    let rulesConfig = [registerUnitDestroyedRule, registerPlayerLostRule, 
+        registerGameFinishedRule,
+        logGameFinishedEvent,
+        registerOrderBuildingRule];
+
+    let campaign = new Campaign(mapSupplier, aiProcessor, rulesConfig, players);
+    
+    return campaign;
+}
+
+describe("GameEngine test", () => {
+    test("should change round", () => {
+        //given
+        let gameEngine = new GameEngine(testCampaignProvider);
+
+        //wnen
+        gameEngine.update();
+
+        //then
+        expect(gameEngine.round).toEqual(1);
+    })
+})
--- src/test/game/engine/rules/GameFinished.spec.ts
+++ src/test/game/engine/rules/GameFinished.spec.ts
@@ -1,18 +1,44 @@
 
+import { AiProcessor } from "../../../../game/engine/Ai/processor/AiProcessor";
+import { Campaign } from "../../../../game/engine/campaign/Campaign";
 import { CampaignFactory, CampaignName } from "../../../../game/engine/campaign/CampaignFactory";
 import { EventChannels } from '../../../../game/engine/events/EventsRegistry';
 import { GameFinishedEventData } from "../../../../game/engine/events/GameEvent";
 import { GameEngine } from "../../../../game/engine/GameEngine";
+import { MapBoard, Terrain, TerrainType } from "../../../../game/engine/map/MapBoard";
+import { Player } from "../../../../game/engine/Player";
+import { registerGameFinishedRule } from "../../../../game/engine/rules/game_finished/GameFinishedRule";
+import { registerPlayerLostRule } from "../../../../game/engine/rules/player_lost/PlayerLostRule";
+import { registerUnitDestroyedRule } from "../../../../game/engine/rules/unit_destroyed/UnitDestroyedRule";
 import { UnitName } from '../../../../game/engine/units/UnitFactory';
 
 
-const campaignFactory = new CampaignFactory();
+const testCampaignProvider = (gameEngine: GameEngine) => {
+    let players = [new Player('1', 'Player1'), new Player('2', 'Bot')];
+
+    let terrain: Terrain = { type: (x: number, y: number) => TerrainType.DEFAULT }
+    let mapSupplier = () => new MapBoard(1, 1, gameEngine, terrain);
+
+    let aiProcessor =  new AiProcessor(gameEngine);
+
+    let rulesConfig = [registerUnitDestroyedRule, registerPlayerLostRule, 
+        registerGameFinishedRule];
+
+    let campaign = new Campaign(mapSupplier, aiProcessor, rulesConfig, players);
+
+    let castle1 = gameEngine.unitFactory.of(UnitName.CASTLE, 20, 20, gameEngine, players[0])
+    let castle2 = gameEngine.unitFactory.of(UnitName.CASTLE, 80, 80, gameEngine, players[1])
+
+    gameEngine.unitStorage.addUnit(castle1);
+    gameEngine.unitStorage.addUnit(castle2);
+    
+    return campaign;
+}
 
 describe("Game Finished tests", () => {
     test('should finish the game when player lost Castle', () => {
         //given
-        let campaignProvider = campaignFactory.get(CampaignName.BASIC_CAMPAIGN);
-        let gameEngine = new GameEngine(campaignProvider);
+        let gameEngine = new GameEngine(testCampaignProvider);
         let player = gameEngine.getPlayer();
         let castleFilter = {
             owner: player,
